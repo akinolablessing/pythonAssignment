cf55f784191216df4b9ab46d4f90bf9abf2cc78a
akinolablessing <akinolablessing1890@gmail.com>
2024-10-16 19:38:40 +0100

    my python assignment

---
+++ assignment/Numbers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+  public class Numbers{
+    public static void main(String[] args){
+      Scanner scanner = new Scanner(System.in);
+
+      System.out.print("Enter 1st number:");
+      int firstNumber = scanner.nextInt();
+
+      System.out.print("Enter 2nd number:");
+      int secondNumber = scanner.nextInt();
+
+      System.out.print("Enter 3rd number:");
+      int thirdNumber = scanner.nextInt();
+
+     System.out.print("Enter 4th number:");
+      int forthNumber = scanner.nextInt();
+
+      System.out.print("Enter 5th number:");
+      int fifthNumber = scanner.nextInt();
+
+     
+ }
+}
